09d6ad5c49c7217edcd1e0a773ef4b1bc00f5e2e
Chris Scott <cscott@problemsolutions.net>
2015-05-19 22:00:41 -0400

    added core.

---
+++ LEM.Core/Attributes/LEMManagerAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEM.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ManagerAttribute:Attribute
+    {
+    }
+}
